0e9789bce4d433b839c8a43c92b34911b2d5a11f
sd2334 <sd2334@njit.edu>
2026-01-04 20:46:02 -0500

    Switch AppSync auth to userPool and fix profileOwner to sub

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,24 +1,22 @@
 import { a, defineData, type ClientSchema } from "@aws-amplify/backend";
 import { postConfirmation } from "../auth/post-confirmation/resource";
+
 const schema = a
- .schema({
- UserProfile: a
- .model({
- email: a.string(),
- profileOwner: a.string(),
- })
- .authorization((allow) => [
- allow.ownerDefinedIn("profileOwner"),
- ]),
- })
- .authorization((allow) => [allow.resource(postConfirmation)]);
+  .schema({
+    UserProfile: a
+      .model({
+        email: a.string(),
+        profileOwner: a.string(),
+      })
+      .authorization((allow) => [allow.ownerDefinedIn("profileOwner")]),
+  })
+  .authorization((allow) => [allow.resource(postConfirmation)]);
+
 export type Schema = ClientSchema<typeof schema>;
+
 export const data = defineData({
- schema,
- authorizationModes: {
- defaultAuthorizationMode: "apiKey",
- apiKeyAuthorizationMode: {
- expiresInDays: 30,
- },
- },
+  schema,
+  authorizationModes: {
+    defaultAuthorizationMode: "userPool",
+  },
 });
